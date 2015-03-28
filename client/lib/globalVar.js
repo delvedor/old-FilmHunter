2ea3ed08e28faa732afaf11418c90603ae43c688
delvedor <tommydelved@gmail.com>
2015-03-28 15:23:22 +0100

    Implemented search by genre, updated logo and favicon.

--- client/lib/globalVar.js
+++ client/lib/globalVar.js
@@ -10,3 +10,4 @@ Session.setDefault('searching', false);
 Session.setDefault('numberOfResults', 0);
 dbResults = new Meteor.Collection(null);
 dbMovieInfo = new Meteor.Collection(null);
+Meteor.subscribe('genres');
